e90885095bf3c3ad609c886314d5fe5d9db79154
YAJATapps <yajatapps3@gmail.com>
2026-08-16 02:54:28 -0400

    apollo: switch to 64-bit only primary zygote target

--- apollo/common/system/apollo_64_bit.mk
+++ apollo/common/system/apollo_64_bit.mk
@@ -5,20 +5,14 @@ TARGET_CPU_VARIANT := generic
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 
-# Secondary Arch
-TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-a
-TARGET_2ND_CPU_ABI := armeabi-v7a
-TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := generic
+
 
 # 64-bit app support
 TARGET_SUPPORTS_64_BIT_APPS := true
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.zygote=zygote64_32
+    ro.zygote=zygote64
 
 PRODUCT_PACKAGES += \
-    init.zygote64.rc \
-    init.zygote64_32.rc
+    init.zygote64.rc
 
