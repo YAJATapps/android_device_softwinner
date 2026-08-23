272c17877495d4526ea6fab4ce56e7c4a77f441f
YAJATapps <yajatapps3@gmail.com>
2026-08-23 18:37:22 -0400

    apollo: misc: update health HAL to android.hardware.health-service.example

--- apollo/common/misc/config.mk
+++ apollo/common/misc/config.mk
@@ -10,8 +10,8 @@ PRODUCT_PACKAGES += \
     com.android.hardware.power \
     android.hardware.lights-service.example
 
-# health HAL (AIDL batteryless for SBC)
+# health HAL (AIDL Health V4)
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.batteryless
+    android.hardware.health-service.example
 
 
