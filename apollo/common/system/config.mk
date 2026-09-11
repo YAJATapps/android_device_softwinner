6aebe38cfb74a0917179ad5a4586a911be09dbe2
YAJATapps <yajatapps3@gmail.com>
2026-09-10 20:51:28 -0400

    apollo: config: remove ro.control_privapp_permissions override
    Remove conflicting ro.control_privapp_permissions=log assignments to
    align with LineageOS enforce policy and prevent duplicate sysprop build
    errors in product-build.prop.

--- apollo/common/system/config.mk
+++ apollo/common/system/config.mk
@@ -74,14 +74,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # system
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.control_privapp_permissions=log \
-    init.userspace_reboot.is_supported=1 \
-
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.control_privapp_permissions=log
-
-PRODUCT_SYSTEM_PROPERTIES += \
-    ro.control_privapp_permissions=log
+    init.userspace_reboot.is_supported=1
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.soc.manufacturer=Allwinner \
